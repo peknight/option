c3cd79df07126f1e6be7fac033c6b00a817e036f
peknight <JKpeknight@gmail.com>
2025-09-06 13:27:19 +0800

    update

--- option-core/shared/src/main/scala/com/peknight/option/OptionKey.scala
+++ option-core/shared/src/main/scala/com/peknight/option/OptionKey.scala
@@ -2,6 +2,7 @@ package com.peknight.option
 
 import cats.Show
 import cats.syntax.eq.*
+import com.peknight.option.ArgumentStyle.SpaceSeparated
 import spire.math.Interval
 
 sealed trait OptionKey:
@@ -12,12 +13,34 @@ sealed trait OptionKey:
   def combinable: Boolean
 end OptionKey
 object OptionKey:
-  case class LongOption(key: String, argumentStyle: ArgumentStyle, private val argLen: Interval[Int], private val canBeCombined: Boolean) extends OptionKey:
+  case class LongOption(key: String, argumentStyle: ArgumentStyle = SpaceSeparated,
+                        private val argLen: Interval[Int] = Interval.point(1)) extends OptionKey:
     def keyType: OptionKeyType = OptionKeyType.LongOption
     def argumentLength: Interval[Int] = argLen & Interval.atOrAbove(0)
-    def combinable: Boolean = canBeCombined && argumentStyle === ArgumentStyle.NoArgument && argumentLength === Interval.point(0)
+    def combinable: Boolean = false
   end LongOption
+  case class ShortOption(private val keyChar: Char, argumentStyle: ArgumentStyle = SpaceSeparated,
+                         private val argLen: Interval[Int] = Interval.point(1),
+                         private val canBeCombined: Boolean = true) extends OptionKey:
+    def key: String = s"$keyChar"
+    def keyType: OptionKeyType = OptionKeyType.ShortOption
+    def argumentLength: Interval[Int] = argLen & Interval.atOrAbove(0)
+    def combinable: Boolean = canBeCombined && argumentLength === Interval.point(0)
+  end ShortOption
+  case class NonStandardOption(key: String, argumentStyle: ArgumentStyle = SpaceSeparated,
+                               private val argLen: Interval[Int] = Interval.point(1)) extends OptionKey:
+    def keyType: OptionKeyType = OptionKeyType.NonStandardOption
+    def argumentLength: Interval[Int] = argLen & Interval.atOrAbove(0)
+    def combinable: Boolean = false
+  end NonStandardOption
+  case class BSDOption(private val keyChar: Char, argumentStyle: ArgumentStyle = SpaceSeparated,
+                       private val argLen: Interval[Int] = Interval.point(0),
+                       private val canBeCombined: Boolean = true) extends OptionKey:
+    def key: String = s"$keyChar"
+    def keyType: OptionKeyType = OptionKeyType.BSDOption
+    def argumentLength: Interval[Int] = argLen & Interval.atOrAbove(0)
+    def combinable: Boolean = canBeCombined && argumentLength === Interval.point(0)
+  end BSDOption
   given showOptionKey: Show[OptionKey] =
-    // TODO
     Show.fromToString[OptionKey]
 end OptionKey
